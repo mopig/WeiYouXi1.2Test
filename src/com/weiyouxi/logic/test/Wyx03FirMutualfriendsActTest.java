f28c7c478ccb91b8a21a3bd4d36493f918997214
mopig <leoaee@gmail.com>
2012-05-23 17:56:41 +0800

    完成 互粉好友页面

--- src/com/weiyouxi/logic/test/Wyx03FirMutualfriendsActTest.java
+++ src/com/weiyouxi/logic/test/Wyx03FirMutualfriendsActTest.java
@@ -12,7 +12,8 @@ import com.jayway.android.robotium.solo.Solo;
 import com.weiyouxi.logic.R;
 import com.weiyouxi.logic.WyxLoginAct;
 import com.weiyouxi.logic.WyxLoginPreAct;
-import com.weiyouxi.logic.WyxMainAct;
+import com.weiyouxi.entity.WyxFirMutualFriends;
+import com.weiyouxi.entity.WyxGameCenter;
 
 @SuppressWarnings("rawtypes")
 public class Wyx03FirMutualfriendsActTest extends ActivityInstrumentationTestCase2{
@@ -30,7 +31,7 @@ public class Wyx03FirMutualfriendsActTest extends ActivityInstrumentationTestCas
 	}
 	
 	public void test01MutualFriends() throws Exception{
-		try {
+//		try {
 			solo.clickOnButton("互粉好友");
 //			solo.clickLongOnText("互粉好友");
 //			solo.clickOnText("设置");
@@ -39,23 +40,51 @@ public class Wyx03FirMutualfriendsActTest extends ActivityInstrumentationTestCas
 //			solo.sleep(5000);
 //			solo.clickOnText("加载更多");
 			solo.sleep(5000);
-			
+			solo.clickOnText("更多");
+			solo.sleep(3000);
 			ArrayList<ListView> ListViews = solo.getCurrentListViews();
 			ListView MutualfriendsListView = ListViews.get(0);
-			View[] MutualfriendsItem = new View[5];
-			TextView[] FriendName = new TextView[5];
-			for (int i = 1; i < 5; i++) {
-				MutualfriendsItem[i] = MutualfriendsListView.getChildAt(i);
-				FriendName[i] = (TextView) MutualfriendsItem[i]
-						.findViewById(R.id.fir_mutualfriends_textview_username);
+			String[] FriendName = new String[17];
+//			for (int i = 1; i < 5; i++) {
+//				MutualfriendsItem[i] = MutualfriendsListView.getChildAt(i);
+//				FriendName[i] = (String) MutualfriendsItem[i]
+//						.findViewById(R.id.fir_mutualfriends_textview_username);
+//
+//				Log.e("好友排行列表", ":" + FriendName[i].getText());
+//			}
+			for (int i = 1; i < 18; i++) {
+				WyxFirMutualFriends gameCenter = (WyxFirMutualFriends) MutualfriendsListView.getItemAtPosition(i);
+				Log.e("互粉好友", "互粉好友"+i+":" + gameCenter.getName());
+				FriendName[i-1] = ((WyxFirMutualFriends) MutualfriendsListView.getItemAtPosition(i)).getName();
+//				GameItem[i] = GameListView2.getChildAt(i);
+//				GameName[i] = (TextView) GameItem[i]
+//						.findViewById(R.id.fir_game_center_listing_item_gamename_tv);
 
-				Log.e("好友排行列表", ":" + FriendName[i].getText());
+//				Log.e("游戏中心列表", "  "+i+":" + GameName[i].getText());
 			}
 			
 			ListView MutualFriendsListView = ListViews.get(0);
 			//查看适配器个数
-			Log.e("item个数", " 就是："+MutualFriendsListView.getAdapter().getCount());
+			Log.e("item个数", " 就是："+MutualFriendsListView.getCount());
 			
+		//验证数据正确性
+			assertEquals("第1个好友不是‘超三国’？", "浅慯辄止", FriendName[0]);
+			assertEquals("第2个好友不是‘超三国’？", "渺飘零", FriendName[1]);
+			assertEquals("第3个好友不是‘超三国’？", "十五代庆喜_dsfdsfdss十五代庆喜", FriendName[2]);
+			assertEquals("第4个好友不是‘超三国’？", "基金_weiyouxi03", FriendName[3]);
+			assertEquals("第5个好友不是‘超三国’？", "weiyouxi_28", FriendName[4]);
+			assertEquals("第6个游戏不是‘超三国’？", "我的微Online测试WWWW加油", FriendName[5]);
+			assertEquals("第7个游戏不是‘超三国’？", "水中太阳空中花", FriendName[6]);
+			assertEquals("第8个游戏不是‘超三国’？", "luo帅哥", FriendName[7]);
+			assertEquals("第9个游戏不是‘超三国’？", "weiyouxi_31", FriendName[8]);
+			assertEquals("第10个游戏不是‘超三国’？", "淘特价吧", FriendName[9]);
+			assertEquals("第11个游戏不是‘超三国’？", "weiyouxi_45", FriendName[10]);
+			assertEquals("第12个游戏不是‘超三国’？", "weiyouxi_34", FriendName[11]);
+			assertEquals("第13个游戏不是‘超三国’？", "weiyouxi_21", FriendName[12]);
+			assertEquals("第14个游戏不是‘超三国’？", "weiyouxi_36", FriendName[13]);
+			assertEquals("第15个游戏不是‘超三国’？", "weiyouxi_40", FriendName[14]);
+			assertEquals("第16个游戏不是‘超三国’？", "weiyouxi_39", FriendName[15]);
+			assertEquals("第17个游戏不是‘超三国’？", "weiyouxi_38", FriendName[16]);
 //			View[] FridendAdapter = new View[friendCount];
 //			FridendAdapter[0] = (View) MutualFriendsListView.getChildAt(0);
 //			View FridendAdapter = (View) MutualFriendsListView.getAdapter().getItem(0);
@@ -77,39 +106,45 @@ public class Wyx03FirMutualfriendsActTest extends ActivityInstrumentationTestCas
 //				Log.e(TAG, "互粉好友名称"+i+"="+ UserName[i]);
 //			}
 //			
-		} catch (Exception e) {
+//		} catch (Exception e) {
 //			// TODO: handle exception
-			assertTrue("艹，互粉好友不能正常显示", false);
+//			assertTrue("艹，互粉好友不能正常显示", false);
 		}
-	}
+//	}
 	//R.id.fir_mygame_textview_gamename
 	public void test02FriendGamesAct() {
 		solo.clickOnButton("互粉好友");
 		solo.sleep(5000);
-		solo.drag(100.f, 100.f, 150.f, 100.f, 5);
+//		solo.drag(100.f, 100.f, 150.f, 100.f, 5);
+//		solo.sleep(3000);
+		
+		solo.clickOnText("浅慯辄止");
 		solo.sleep(5000);
+		try {
+			ArrayList<ListView> ListViews = solo.getCurrentListViews();
+			ListView FriendGameListView = ListViews.get(0);
+			
+			View[] FriendGameItem = new View[4];
+			
+			TextView[] FriendGameName = new TextView[3];
+//			if(mygamecount==1){
+//				
+//			}else{
+//				assertEquals("我的游戏没有取到数据", true, false);
+//			}
+//			MyGameListView.get
+			Log.e("我的游戏列表", "游戏个数"+FriendGameListView.getCount());
+			Log.e("我的游戏列表", "游戏个数"+FriendGameListView.getItemAtPosition(0).getClass());
+//			try {
+				for(int i = 1;i < 9;i++){
+					FriendGameItem[i] = FriendGameListView.getChildAt(i);
+					FriendGameName[i-1]  = (TextView) FriendGameItem[i].findViewById(R.id.fir_mygame_textview_gamename);
+				}
+		} catch (Exception e) {
+			// TODO: handle exception
+			assertTrue("‘TA的游戏没有数据’", false);
+		}
 		
-		solo.clickOnText("weiyouxi04");
-		solo.sleep(5000);
-		
-		ArrayList<ListView> ListViews = solo.getCurrentListViews();
-		ListView MyGameListView = ListViews.get(0);
-		
-		View[] FriendGameItem = new View[5];
-		
-		TextView[] FriendGameName = new TextView[5];
-//		if(mygamecount==1){
-//			
-//		}else{
-//			assertEquals("我的游戏没有取到数据", true, false);
-//		}
-//		MyGameListView.get
-		Log.e("我的游戏列表", "游戏个数"+MyGameListView.getChildCount());
-//		try {
-			for(int i = 1;i < 5;i++){
-				FriendGameItem[i] = MyGameListView.getChildAt(i);
-				FriendGameName[i]  = (TextView) FriendGameItem[i].findViewById(R.id.fir_mygame_textview_gamename);
-			}
 		
 		
 	}
